3a7fb53e0da80243dc5a4e5db58c39699dbfd9a3
LeoChen <leo.chen@shoalter.com>
2021-07-26 16:20:49 +0800

    Add react router dom plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import Counter from "./components/Counter";
+import { increase, decrease, increaseAsync } from "./store/slices/counter";
+
 function App() {
-  return <main>app</main>;
+  const dispatch = useAppDispatch();
+  const value = useAppSelector((state) => state.counter);
+
+  const onIncrement = (): void => {
+    dispatch(increase());
+  };
+
+  const onDecrement = (): void => {
+    dispatch(decrease());
+  };
+
+  const onIncrementAsync = (): void => {
+    dispatch(increaseAsync());
+  };
+
+  return (
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">TodoList</Link>
+              </li>
+              <li>
+                <Link to="/counter">Counter</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/counter">
+              <Counter
+                value={value}
+                onIncrement={onIncrement}
+                onDecrement={onDecrement}
+                onIncrementAsync={onIncrementAsync}
+              />
+            </Route>
+            <Route path="/">
+              <p>todoList</p>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
